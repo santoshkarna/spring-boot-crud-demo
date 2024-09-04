575d34d0f8d25cc4a16a80fcede3990745bee02c
Santosh Karna <santoshkarna@gmail.com>
2024-09-04 06:22:23 +0545

    added again doc

--- src/main/java/com/cubic/springbootcruddemo/repository/UserRepository.java
+++ src/main/java/com/cubic/springbootcruddemo/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.cubic.springbootcruddemo.repository;
 import com.cubic.springbootcruddemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This is user repository
+ */
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
